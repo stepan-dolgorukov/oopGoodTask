4349d98b6eda526d7645c2ce21f0b267a4f8146c
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2022-11-25 10:59:50 +0500

    t2_d2_c2

--- src/main/java/ru/oop/Bus.java
+++ src/main/java/ru/oop/Bus.java
@@ -1,5 +1,8 @@
 package ru.oop;
 
+/**
+ * Автобус.
+ */
 public class Bus implements PassengerTransport, Positioned {
     private Position position_;
     private String route_;
@@ -11,18 +14,32 @@ public class Bus implements PassengerTransport, Positioned {
     public Bus(String route) {
     }
 
+    /**
+     * Довезти пассажира до места назначения.
+     * Если маршрут не проходит через конечный пункт,
+     * довезти до ближайшей к пункту остановки.
+     */
     public void takeClosest(Person passenger, Position destination) {
 
     }
 
+    /**
+     * Получить текущее месторасположение.
+     */
     public Position getPosition() {
         return position_;
     }
 
+    /**
+     * Получить название маршрута.
+     */
     public String getRoute() {
         return route_;
     }
 
+    /**
+     * Обновить месторасположение.
+     */
     public void setPosition(Position position) {
 
     }
